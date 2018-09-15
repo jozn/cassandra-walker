ba2cfd624c0747927e0e9a5dc4c29203720d5cdf
Hamid <7atashfeshan@gmail.com>
2018-09-15 10:41:17 +0430

    update sample output

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ const (
 )
 
 func main() {
-
 	args := &cwalker.ConfigArgs{}
 	arg.MustParse(args)
 	if args.Host == "" {
--- samples/walker_out/zc_models.go
+++ samples/walker_out/zc_models.go
@@ -1,7 +1,46 @@
 package xc
 
-// logs tables
-type LogTableCql struct {
+type Twitt struct {
+	Body       string // body  regular
+	CreateTime int    // create_time  regular
+	TwiitId    string // twiit_id  clustering
+	UserId     int    // user_id  partition_key
+
+	_exists, _deleted bool
 }
 
-var LogTableCqlReq = LogTableCql{}
+/*
+:= &xc.Twitt {
+	Body: "",
+	CreateTime: 0,
+	TwiitId: "",
+	UserId: 0,
+*/
+
+type User struct {
+	CreatedTime int    // created_time  regular
+	FullName    string // full_name  regular
+	UserId      int    // user_id  partition_key
+	UserName    string // user_name  regular
+
+	_exists, _deleted bool
+}
+
+/*
+:= &xc.User {
+	CreatedTime: 0,
+	FullName: "",
+	UserId: 0,
+	UserName: "",
+*/
+
+// logs tables
+type LogTableCql struct {
+	Twitt bool
+	User  bool
+}
+
+var LogTableCqlReq = LogTableCql{
+	Twitt: true,
+	User:  true,
+}
